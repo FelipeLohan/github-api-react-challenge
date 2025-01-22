c88daaae66d9c43fe7d708f9da98388f67cf9718
Felipe Lohan <felipelohan03052006@gmail.com>
2025-01-22 09:19:33 -0300

    Maked Forms Style

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,6 +3,34 @@ import { Button } from "../Button";
 import { Input } from "../Input";
 import * as githubApi from "../../services/githubApi.ts";
 import { ProfileResult } from "../ProfileResult/ProfileResult.tsx";
+import styled from "styled-components";
+
+const FormContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 80%;
+  margin: 0 auto;
+  background-color: #E6F7FF;
+  padding: 40px;
+  border-radius: 12px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  
+  h2{
+    font-size: 3.5vmin;
+    color: #262626;
+  }
+
+  div {
+    display: flex;
+    gap: 10px;
+  }
+
+  div input{
+    width: 20%;
+  }
+`;
 
 const Form = () => {
   type FormData = {
@@ -15,11 +43,11 @@ const Form = () => {
 
   const [resultData, setResultData] = useState({
     avatarUrl: "",
-      followers: "",
-      location: "",
-      name: "",
-      profileUrl: "",
-  })
+    followers: "",
+    location: "",
+    name: "",
+    profileUrl: "",
+  });
 
   async function handleButtonClick(e: any) {
     e.preventDefault();
@@ -37,10 +65,10 @@ const Form = () => {
       followers: result.data.followers,
       location: result.data.location,
       name: result.data.name,
-      profileUrl: result.data.html_url, 
+      profileUrl: result.data.html_url,
     };
 
-    setResultData(ResultData)
+    setResultData(ResultData);
   }
 
   function handleInputChange(e: any) {
@@ -50,27 +78,29 @@ const Form = () => {
 
   return (
     <>
-      <h2>Encontre um perfil Github</h2>
-      <Input
-        inputType=""
-        inputPlaceholder="Usuário Github"
-        name="username"
-        handleOnChange={handleInputChange}
-        value={usernameValue.username}
-      />
-      <Button text="Encontrar" handleOnClick={handleButtonClick} />
+      <FormContainer>
+        <h2>Encontre um perfil Github</h2>
+        <div>
+          <Input
+            inputType=""
+            inputPlaceholder="Usuário Github"
+            name="username"
+            handleOnChange={handleInputChange}
+            value={usernameValue.username}
+          />
+          <Button text="Encontrar" handleOnClick={handleButtonClick} />
+        </div>
+      </FormContainer>
 
-      {
-
-        resultData.name && 
+      {resultData.name && (
         <ProfileResult
-        imageSource={resultData.avatarUrl}
-        profileUrl={resultData.profileUrl}
-        followersNumber={resultData.followers}
-        localization={resultData.location}
-        name={resultData.name}
+          imageSource={resultData.avatarUrl}
+          profileUrl={resultData.profileUrl}
+          followersNumber={resultData.followers}
+          localization={resultData.location}
+          name={resultData.name}
         />
-      }
+      )}
     </>
   );
 };
